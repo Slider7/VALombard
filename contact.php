88ec5456b9b082594f33c3a5bda4247e8cebdb0c
Kemalkhan Shlembayev <kemalsh771@gmail.com>
2018-10-03 16:02:12 +0600

    finished contact form

---
+++ contact.php
@@ -0,0 +1,51 @@
+<?php
+ini_set( 'SMTP', 'smtp.telecom.kz' ); // must be set to your own local ISP
+ini_set( 'smtp_port', '25' ); // assumes no authentication (passwords) required 
+ini_set( 'sendmail_from', 'VA-lombard@site.kz' ); // can be any e-mail address, but must be set
+
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+        # FIX: Replace this email with recipient email
+        $mail_to = "demo@gmail.com";
+        
+        # Sender Data
+        $subject = "Сообщение от посетителя сайта VA Lombard";
+        $name = str_replace(array("\r","\n"),array(" "," ") , strip_tags(trim($_POST["name"])));
+        $email = "";/*filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);*/
+        $message = trim($_POST["message"]);
+        
+        if ( empty($name) OR empty($message)) {
+            # Set a 400 (bad request) response code and exit.
+            http_response_code(400);
+            echo "Заполните форму и попробуйте отправить еще раз.";
+            exit;
+        }
+        
+        # Mail Content
+        $content = "Name: $name\n";
+        $content .= "Email: $email\n\n";
+        $content .= "Phone: $phone\n";
+        $content .= "Message:\n$message\n";
+
+        # email headers.
+        $headers = "From: $name &lt;$email&gt;";
+
+        # Send the email.
+        $success = mail($mail_to, $subject, $content, $headers);
+        if ($success) {
+            # Set a 200 (okay) response code.
+            http_response_code(200);
+            echo "Спасибо! Ваше сообщение успешно отправлено.";
+        } else {
+            # Set a 500 (internal server error) response code.
+            http_response_code(500);
+            echo "Ошибка! Something went wrong, we couldn't send your message.";
+        }
+
+        } else {
+            # Not a POST request, set a 403 (forbidden) response code.
+            http_response_code(403);
+            echo "There was a problem with your submission, please try again.";
+        }
+?>
